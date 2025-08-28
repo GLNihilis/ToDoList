3e165620c861a03e3aaaeea36ccb2ad346cfb497
Nguyễn Đức Anh <Nihilis9999@gmail.com>
2025-08-28 17:00:26 +0700

    Name
    Change folder name

--- ToDoApi/api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ ToDoApi/api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f84731d9ad275137fb1246c5bb0f8161b45b831")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
